7eac4e3a271d40cf773c090397435c1dda0de98f
andybuki <andrey.bukhman@gmail.com>
2026-05-04 16:50:20 +0200

    Add recognition mode preference state

---
+++ composeApp/src/commonMain/kotlin/com/banking/statement/categorization/RecognitionModePreferenceState.kt
@@ -0,0 +1,26 @@
+package com.banking.statement.categorization
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class RecognitionModePreferenceState(
+    initialMode: RecognitionMode,
+    private val persistMode: (RecognitionMode) -> Unit
+) {
+    var mode: RecognitionMode by mutableStateOf(initialMode)
+        private set
+
+    fun setMode(newMode: RecognitionMode) {
+        mode = newMode
+        persistMode(newMode)
+    }
+}
+
+@Composable
+expect fun rememberRecognitionModePreferenceState(): RecognitionModePreferenceState
+
+fun recognitionModeFromName(modeName: String?): RecognitionMode {
+    return RecognitionMode.entries.find { it.name == modeName } ?: RecognitionMode.SAFE
+}
