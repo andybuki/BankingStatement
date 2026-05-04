b5865a69933ba2cf8497a6823e35dd768fcb9aff
andybuki <andrey.bukhman@gmail.com>
2026-05-04 17:01:38 +0200

    Avoid Koin dependency in Android recognition settings state

--- composeApp/src/androidMain/kotlin/com/banking/statement/categorization/RecognitionModePreferenceState.android.kt
+++ composeApp/src/androidMain/kotlin/com/banking/statement/categorization/RecognitionModePreferenceState.android.kt
@@ -2,13 +2,14 @@ package com.banking.statement.categorization
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import com.banking.statement.AppPreferences
-import org.koin.compose.koinInject
 
 @Composable
 actual fun rememberRecognitionModePreferenceState(): RecognitionModePreferenceState {
-    val preferences: AppPreferences = koinInject()
-    return remember(preferences) {
+    val context = LocalContext.current
+    return remember(context) {
+        val preferences = AppPreferences(context)
         RecognitionModePreferenceState(
             initialMode = recognitionModeFromName(preferences.getRecognitionModeName()),
             persistMode = { mode -> preferences.setRecognitionModeName(mode.name) }
